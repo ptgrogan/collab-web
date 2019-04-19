a222e3b5d35b1ba85e504c6ae15fd17638165b03
Paul Grogan <pgrogan@stevens.edu>
2019-04-19 15:18:56 -0400

    fix division

--- python/processor.py
+++ python/processor.py
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+from __future__ import division
 import argparse
 import json
 import numpy as np
